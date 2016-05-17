f08a814cbb5c26f4ebbae0c1348a07c4ed05ec15
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-05-17 18:20:06 -0500

    Add placeholder files.

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'six>=1.6.1'
+]
+
+setup(
+    name='leather',
+    version='0.0.1',
+    description='Python charting for 80% of humans.',
+    long_description=open('README.rst').read(),
+    author='Christopher Groskopf',
+    author_email='chrisgroskopf@gmail.com',
+    # url='http://agate.readthedocs.org/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'leather',
+        'leather.scales',
+        'leather.shapes'
+    ],
+    install_requires=install_requires
+)
