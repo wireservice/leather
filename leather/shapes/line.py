d25a42badce05ac2ae819240c482a8ca30ad6150
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-05-17 17:13:43 -0500

    Draw line.

--- leather/shapes/line.py
+++ leather/shapes/line.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python
 
+import xml.etree.ElementTree as ET
+
+import six
+
 from leather.shapes.base import Shape
 
 class Line(Shape):
     """
     Render a series of data as a line.
     """
-    def to_svg(self):
-        pass
+    def __init__(self, width=2, color='blue'):
+        self.width = width
+        self.color = color
+
+    def to_svg(self, bbox, x_scale, y_scale, series):
+        path = ET.Element('path',
+            stroke=self.color,
+            fill='none'
+        )
+
+        d = []
+
+        for x, y in series.data:
+            proj_x = x_scale.project(x, [bbox.left, bbox.right])
+            proj_y = y_scale.project(y, [bbox.bottom, bbox.top])
+
+            if not d:
+                command = 'M'
+            else:
+                command = 'L'
+
+            d.extend([
+                command,
+                six.text_type(proj_x),
+                six.text_type(proj_y)
+            ])
+
+        path.set('d', ','.join(d))
+
+        return path
--- test.py
+++ test.py
@@ -2,13 +2,22 @@
 
 import leather
 
-data = [
+dot_data = [
     (0, 3),
     (4, 5),
     (7, 9),
     (8, 4)
 ]
 
+line_data = [
+    (0, 4),
+    (1, 3),
+    (2, 5),
+    (5, 6),
+    (9, 10)
+]
+
 chart = leather.Chart()
-chart.add_dot(data)
+chart.add_dot(dot_data)
+chart.add_line(line_data)
 chart.to_svg('test.svg')
