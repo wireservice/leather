93398b134fe9ab6ddb55ded6438375df8a7eacf8
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-05-20 15:04:40 -0500

    Rev to 0.3.0 for dev.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,14 @@
-0.1.0
+0.3.0
+-----
+
+
+
+0.2.0
 -----
 
 * Initial prototype
+
+0.1.0
+-----
+
+* Never released
--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,8 @@ master_doc = 'index'
 # Metadata
 project = u'leather'
 copyright = u'2016, Christopher Groskopf'
-version = '0.1.0'
-release = '0.1.0'
+version = '0.3.0'
+release = '0.3.0'
 
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
